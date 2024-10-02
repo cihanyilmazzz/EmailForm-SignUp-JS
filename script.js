e25cf344a747c74e1032417e2ddeb298c493bc8b
Cihan Yılmaz <chnrzgr@gmail.com>
2024-10-02 22:17:07 +0200

    Email character limit was added, timer added for validation message and email autofill preventation was added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Sign Up Form - Email address validation 
+// Email input
 const emailInput = document.getElementById("email");
 const emailInput2 = document.getElementById("email2");
+// Confirmation and Error message
 const confirmMessage = document.getElementById("confirm-message");
-const errorMessage = document.getElementById("error");
+const errorMessage = document.getElementById("email-validation");
+const charMessage = document.getElementById("char-message");
+const charLimit = 50;
 
 const submitBtn = document.querySelector(".submit-btn"); 
 
@@ -12,32 +16,57 @@ submitBtn.addEventListener("click", (e)=> {
     e.preventDefault();
     const email = emailInput.value;
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const emailLength = emailInput.value.length;
 
-    if(emailPattern.test(email)) {
+    // Email validation
+    if (emailPattern.test(email)) {
         errorMessage.textContent = "Valid email";
         errorMessage.style.color = "green";
     } 
     else {
         errorMessage.textContent = "Please enter a valid email address.";
         errorMessage.style.color = "red";
-       
     }
 
+    // Email character limit
+    if (emailLength > charLimit) {
+        charMessage.textContent = "Email cannot exceed 50 characters";
+        emailInput.value = emailLength.value.substring(0, charLimit);
+    }
+    // Removing validation message
+    setTimeout(() => {
+        errorMessage.style.display = "none";
+        charMessage.style.display = "none";
+      }, "2000");
+    if(errorMessage.style.display == "none" || charMessage.style.display == "none") {
+        errorMessage.style.display = "block";
+        charMessage.style.display = "block";
+    }
+ 
 });
 
+// Email confirmation check
 submitBtn.addEventListener("click", (e) => {
     // Prevent form from Submitting
     e.preventDefault();
     const add1 = emailInput.value;
     const add2 = emailInput2.value;
     
-    if(add1 === add2) {
+    if (add1 === add2) {
         confirmMessage.textContent= "Emails match!";
         confirmMessage.style.color = "green";
     } 
     else {
         confirmMessage.textContent = "Emails don't match!";
         confirmMessage.style.color = "red";
+    }
+    // Removing validation message
+    setTimeout(() => {
+        confirmMessage.style.display = "none";
+      }, "2000");
+
+    if(confirmMessage.style.display = "none") {
+        confirmMessage.style.display = "block";
     }
 })
 
