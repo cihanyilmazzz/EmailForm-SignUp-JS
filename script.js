773f55a2bae95b2a74a46b07e935fa15395e77f7
Cihan Yılmaz <chnrzgr@gmail.com>
2024-10-03 00:12:04 +0200

    form style update

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 // Sign Up Form - Email address validation 
-// Email input
+// Email form elements
 const emailInput = document.getElementById("email");
 const emailInput2 = document.getElementById("email2");
-// Confirmation and Error message
+const submitBtn = document.querySelector(".submit-btn"); 
+const suggestionBox = document.getElementById("suggestion-box");
+const pwd = document.getElementById("pwd");
+const feedback = document.getElementById("email-feedback");
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+// Confirmation & Error message
 const confirmMessage = document.getElementById("confirm-message");
 const errorMessage = document.getElementById("email-validation");
 const charMessage = document.getElementById("char-message");
+
+// Email character limit
 const charLimit = 50;
 
-const submitBtn = document.querySelector(".submit-btn"); 
-
+// Real time Email address validation
+emailInput.addEventListener("input", function() {
+    if(emailPattern.test(emailInput.value)) {
+        feedback.textContent = "Valid email";
+        feedback.style.color = "green";
+    }
+    else {
+        feedback.textContent = "Invalid email";
+        feedback.style.color = "red";
+    }
+});
 
 submitBtn.addEventListener("click", (e)=> {
     // Prevent form from Submitting
     e.preventDefault();
     const email = emailInput.value;
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const emailLength = emailInput.value.length;
-
-    // Email validation
-    if (emailPattern.test(email)) {
-        errorMessage.textContent = "Valid email";
-        errorMessage.style.color = "green";
-    } 
-    else {
-        errorMessage.textContent = "Please enter a valid email address.";
-        errorMessage.style.color = "red";
-    }
 
     // Email character limit
     if (emailLength > charLimit) {
         charMessage.textContent = "Email cannot exceed 50 characters";
         emailInput.value = emailLength.value.substring(0, charLimit);
     }
+
     // Removing validation message
     setTimeout(() => {
         errorMessage.style.display = "none";
@@ -42,7 +49,6 @@ submitBtn.addEventListener("click", (e)=> {
         errorMessage.style.display = "block";
         charMessage.style.display = "block";
     }
- 
 });
 
 // Email confirmation check
@@ -52,6 +58,7 @@ submitBtn.addEventListener("click", (e) => {
     const add1 = emailInput.value;
     const add2 = emailInput2.value;
     
+    // Email matching validation
     if (add1 === add2) {
         confirmMessage.textContent= "Emails match!";
         confirmMessage.style.color = "green";
@@ -60,6 +67,7 @@ submitBtn.addEventListener("click", (e) => {
         confirmMessage.textContent = "Emails don't match!";
         confirmMessage.style.color = "red";
     }
+
     // Removing validation message
     setTimeout(() => {
         confirmMessage.style.display = "none";
@@ -68,5 +76,6 @@ submitBtn.addEventListener("click", (e) => {
     if(confirmMessage.style.display = "none") {
         confirmMessage.style.display = "block";
     }
-})
+});
+
 
